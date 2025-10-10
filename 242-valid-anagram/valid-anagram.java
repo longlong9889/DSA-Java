30b7db6fcbfeaf177c8117a012a255bb8fe3a3b4
longlong9889 <longhuyhoangnguyen@csus.edu>
2025-10-10 12:19:04 -0700

    Time: 17 ms (17.22%) | Memory: 44.8 MB (25.68%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char b = t.charAt(i); 
+            map.put(b, map.getOrDefault(b, 0) - 1);
+            if (map.getOrDefault(b, 0) == 0) {
+                map.remove(b);
+            }
+        }
+        if (map.isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
