bf15dda01531c1fb9429eed25f7bd4bb89decbaa
longlong9889 <longhuyhoangnguyen@csus.edu>
2025-12-11 13:32:24 -0800

    Time: 6 ms (98.92%) | Memory: 49.6 MB (38.19%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str: strs) {
+            char[] anagram = str.toCharArray();
+            Arrays.sort(anagram);
+            String key = new String(anagram);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
