95457bc4aec9b1e6403ca773f8b6bd1a7d5711aa
longlong9889 <longhuyhoangnguyen@csus.edu>
2025-12-25 13:51:18 -0800

    Time: 1 ms (99.79%) | Memory: 79.4 MB (10.07%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,9 +3,9 @@ class Solution {
         if (nums.length == 1) return nums[0];
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        int max = nums[0];
+        int max = dp[0];
         for (int i = 1; i < nums.length; i++) {
-            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
             max = Math.max(dp[i], max);
         }
         return max;
