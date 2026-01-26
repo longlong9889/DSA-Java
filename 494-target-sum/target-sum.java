3bd220cae72f75471c55f90ecd9c789fc822ce8d
longlong9889 <longhuyhoangnguyen@csus.edu>
2026-01-26 15:26:09 -0800

    Time: 63 ms (43.25%) | Memory: 47 MB (22.76%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,15 +1,15 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         Map<Integer, Integer> dp = new HashMap<>();
-        dp.put(0,1);
+        dp.put(0, 1);
         for (int num: nums) {
-            Map<Integer, Integer> dpNew = new HashMap<>();
+            Map<Integer, Integer> newdp = new HashMap<>();
             for (int sum: dp.keySet()) {
                 int count = dp.get(sum);
-                dpNew.put(sum + num, dpNew.getOrDefault(sum + num, 0) + count);
-                dpNew.put(sum - num, dpNew.getOrDefault(sum - num, 0) + count);
+                newdp.put(sum + num, newdp.getOrDefault(sum + num, 0) + count);
+                newdp.put(sum - num, newdp.getOrDefault(sum - num, 0) + count);
             }
-            dp = dpNew;
+            dp = newdp;
         }
         return dp.getOrDefault(target, 0);
     }
